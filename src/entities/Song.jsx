f9bf8d29d3b80c300d341fec1f35d07d65244e4d
septiarmustafa <septiarmustafa@gmail.com>
2024-06-21 11:06:25 +0700

    Entities Song

---
+++ src/entities/Song.jsx
@@ -0,0 +1,9 @@
+export default class Song {
+    constructor(id, title, artist, album, artworkUrl) {
+      this.id = id;
+      this.title = title;
+      this.artist = artist;
+      this.album = album;
+      this.artworkUrl = artworkUrl;
+    }
+  }
